798f420619c4a6d0baad6a74d97ef6f4826eba4b
Lisa Angermann <lisa.angermann@gfz-potsdam.de>
2014-05-29 19:37:20 +0200

    congress implementation version 1.0

--- ticktacktoe.ino
+++ ticktacktoe.ino
@@ -6,42 +6,53 @@
 This code is published under GPL v2
 */
 
+
+#include <DmxSimple.h>
+
 // constants won't change. Used here to 
 // set pin numbers:
-const int RedledPin     = 2;      // the number of the red LED pin
-const int GreenledPin   = 3;      // the number of the green LED pin
-const int SensorPins[9] = {A0, A1, A2, A3, A4, A5, A6, A7, A7};
+const int RedledPin     = 65;      // the number of the red LED pin
+const int GreenledPin   = 66;      // the number of the green LED pin
+const int SensorPins[9] = {A13, A14, A2, A3, A4, A5, A10, A11, A12};
+const int FieldAddr[9]  = {2, 5, 8, 29, 14, 17, 20, 23, 26};
 
 const int Threshold   = 200;    // trigger value to switch LED on (from analog signal)
-const int TimeOut     = 15000;  // number of loops needed for positive input signal 
+const int TimeOut     = 1000;  // number of loops needed for positive input signal 
 // game states
 const int Reset       = 0;
 const int RedTurn     = 1;       
 const int GreenTurn   = 2;
 const int RedWon      = 3;
 const int GreenWon    = 4;
+const int Idle        = 5;
 // field states
 const int FieldFree   = 0;
 const int FieldRed    = 1;
 const int FieldGreen  = 2;
 
+const int MaxBrightness  = 250;
+
 
 // Variables will change:
 int sensorValue;     // variable to store the value coming from the sensor, possible Range 0 - 1023
-int redledState;         // ledState used to set the red LED
-int greenledState;       // ledState used to set the green LED
 int gameState = Reset;
 int count[9]  = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 int fieldState[9];
-
+int idleCount;
 
 void setup() 
 {
+  DmxSimple.usePin(3);
+  DmxSimple.maxChannel(64);
+
   pinMode(RedledPin, OUTPUT);      
   pinMode(GreenledPin, OUTPUT);  
   
   for (int i = 0; i < 9; i++) 
-    pinMode(SensorPins[i], INPUT);      
+    pinMode(SensorPins[i], INPUT);    
+
+  DmxSimple.write(1, 255); // switch master on 
+  gameState = Idle;
 }
 
 int checkTriplet (int a, int b, int c) 
@@ -56,30 +67,71 @@ int displayState (int state)
 {
   switch (state) {
     case RedTurn:
-      redledState   = HIGH;
-      greenledState = LOW;
+      DmxSimple.write(RedledPin, MaxBrightness);
+      DmxSimple.write(GreenledPin, 0);
       break;
 
     case GreenTurn:
-      greenledState = HIGH;
-      redledState   = LOW;
+      DmxSimple.write(GreenledPin, MaxBrightness);
+      DmxSimple.write(RedledPin, 0);
       break;
   }
 }
 
+int celebrate (int winnerFieldState) {
+   for (int z = 0; z < 15; z++) {
+     DmxSimple.write(RedledPin, MaxBrightness);
+     DmxSimple.write(GreenledPin, 0);
+     for (int i = 0; i < 9; i++) {
+        delay(50);
+        switchLamp(i, FieldFree, 0);
+     }
+
+     delay(100);
+   
+     for (int i = 0; i < 9; i++) 
+        switchLamp(i, winnerFieldState, MaxBrightness);
+   
+     DmxSimple.write(RedledPin, 0);
+     DmxSimple.write(GreenledPin, MaxBrightness);
+     delay(300);
+   }
+   
+  switchAllOff();
+}
+
+
 // switch on red or green light, has to be adapted to the actual setting...
-int switchLamp (int index, int newFieldState) 
+int switchLamp (int index, int newFieldState, int brightness)
 {
-  
+    switch (newFieldState) {
+       case FieldFree:
+          DmxSimple.write(FieldAddr[index], 0);
+          DmxSimple.write(FieldAddr[index] + 1, 0);
+          break;
+          
+       case FieldRed:
+          DmxSimple.write(FieldAddr[index], brightness);
+          DmxSimple.write(FieldAddr[index] + 1, 0);
+          break;
+          
+       case FieldGreen:
+          DmxSimple.write(FieldAddr[index], 0);
+          DmxSimple.write(FieldAddr[index] + 1, brightness);
+          break;
+    }
 }
 
 int checkFields(int newFieldState) 
 {
-    int fieldValue = checkSensor();
+    int fieldValue = checkSensor(newFieldState);
     if (fieldValue >= 0 && fieldState[fieldValue] == FieldFree) {
       fieldState[fieldValue] = newFieldState;
-      switchLamp(fieldValue, newFieldState);
+      switchLamp(fieldValue, newFieldState, MaxBrightness);
+      return 1;
     }
+    
+    return 0;
 }
 
 int checkWinner() 
@@ -94,7 +146,16 @@ int checkWinner()
          checkTriplet(2, 4, 6);
 }
 
-int checkSensor()
+int checkGameOver() 
+{
+  for (int i = 0; i < 9; i++) {
+    if (fieldState[i] == FieldFree)
+      return 0;
+  }
+  return 1;
+}
+
+int checkSensor(int newFieldState)
 {
   for (int i = 0; i < 9; i++) {
     sensorValue = analogRead(SensorPins[i]); 
@@ -103,55 +164,93 @@ int checkSensor()
     else 
       count[i] = 0;
 
+    if (fieldState[i] == FieldFree && newFieldState != FieldFree)
+      switchLamp(i, newFieldState, (count[i] * MaxBrightness) / (2 *TimeOut));
+
     if (count[i] > TimeOut) {
       count[i] = 0;
       return i;
     } 
   }
   return -1; 
-  
 }
 
-  
+void switchAllOff()
+{
+  for (int i = 0; i < 9; i++)
+    switchLamp(i, FieldFree, 0);
 
+  DmxSimple.write(RedledPin, 0);
+  DmxSimple.write(GreenledPin, 0);
+}
 
 void loop()
 {
+//  celebrate(FieldRed);
+//  return;
+
   switch (gameState) {
     case Reset:
       for (int i = 0; i < 9; i++)
         fieldState[i] = FieldFree;
+
+      switchAllOff();
       gameState = RedTurn;
       break;
 
-    case RedTurn:
-      displayState(RedTurn);
-      checkFields(FieldRed);   
+    case Idle:
+      if (checkSensor(FieldFree) == 4) {
+        gameState = Reset;
+        break; 
+      }
+      
+      if (idleCount++ % 500 == 0) {
+        int lamp = random(8);
+        
+        if (lamp == 4)
+          lamp++;
+          
+        switchLamp(lamp, random(3), MaxBrightness);
+      }
+      
+      break;
     
-      if (checkWinner())
-        gameState = RedWon;
-      else
-        gameState = GreenTurn;
+    case RedTurn:
+     displayState(RedTurn);
+     if (checkFields(FieldRed)) {
+        if (checkWinner())
+          gameState = RedWon;
+        else if (checkGameOver()) {
+          switchAllOff();
+          gameState = Idle;
+        } else
+          gameState = GreenTurn;
+      }
       break;
 
     case GreenTurn:
       displayState(GreenTurn);
-      checkFields(FieldGreen);   
-
-      if (checkWinner())
-        gameState = GreenWon;
-      else
-        gameState = RedTurn;
+      if (checkFields(FieldGreen)) {
+        if (checkWinner())
+           gameState = GreenWon;
+        else if (checkGameOver()) {
+          switchAllOff();
+          gameState = Reset;
+        } else
+          gameState = RedTurn;
+      }
       break;
 
     case RedWon:
+      celebrate(FieldRed);
+      gameState = Idle;
+      break;
+      
     case GreenWon:
-      gameState = Reset;
+      celebrate(FieldGreen);
+      gameState = Idle;
       break;
   }  
-
-  digitalWrite(GreenledPin, greenledState);
-  digitalWrite(RedledPin, redledState);
 } 
 
 
